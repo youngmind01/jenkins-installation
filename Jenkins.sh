ea4a8503fbbe332bcda18cbd103eea6814a8e5a7
Promise <54754559+youngmind01@users.noreply.github.com>
2023-04-17 21:14:23 +0100

    install script

---
+++ Jenkins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt update
+sudo apt install openjdk-11-jdk -y
+sudo apt install maven -y
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update 
+
+sudo apt-get install jenkins -y
